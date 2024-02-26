2d2bdc3c8f52e1fef0eef60a4606ae28078563c3
NeilTheSeal <neil.hendren@gmail.com>
2024-02-26 09:37:02 -0700

    Add test for current highest bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -32,6 +32,11 @@ RSpec.describe Item do
         @attendee2 => 22
       })
     end
+    it "can list the highest bid" do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.current_high_bid).to eq(22)
+    end
   end
 end
 # rubocop:enable Metrics/BlockLength
