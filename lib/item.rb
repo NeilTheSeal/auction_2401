c2ef29ec6ee1187e1dfa36a2a0a23558c0f9037c
NeilTheSeal <neil.hendren@gmail.com>
2024-02-26 10:52:21 -0700

    Edit item class to pass close bidding test

--- lib/item.rb
+++ lib/item.rb
@@ -4,13 +4,22 @@ class Item
   def initialize(name)
     @name = name
     @bids = {}
+    @closed = false
   end
 
   def add_bid(attendee, bid)
-    @bids[attendee] = bid
+    @bids[attendee] = bid unless @closed
   end
 
   def current_high_bid
     @bids.values.max || 0
+  end
+
+  def close_bidding
+    if !@closed
+      @closed = true
+    else
+      false
+    end
   end
 end
