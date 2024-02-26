257e46128867915f9bd46fd041b16f4e1dfce1a1
NeilTheSeal <neil.hendren@gmail.com>
2024-02-26 09:16:41 -0700

    Add existence test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,5 +2,13 @@ require "./spec/spec_helper"
 
 RSpec.describe Item do
   before(:each) do
+    @item1 = Item.new("Chalkware Piggy Bank")
+    @item2 = Item.new("Bamboo Picture Frame")
+  end
+
+  describe "#Initialization - " do
+    it "exists" do
+      expect(@item1).to be_an_instance_of(Item)
+    end
   end
 end
