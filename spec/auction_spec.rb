ec5636a055417786437d699f2ff06f2a5b82640a
NeilTheSeal <neil.hendren@gmail.com>
2024-02-26 09:23:29 -0700

    Add existence spec

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -2,5 +2,12 @@ require "./spec/spec_helper"
 
 RSpec.describe Auction do
   before(:each) do
+    @auction = Auction.new
+  end
+
+  describe "#Initialization - " do
+    it "exists" do
+      expect(@auction).to be_an_instance_of(Auction)
+    end
   end
 end
