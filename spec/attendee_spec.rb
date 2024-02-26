122b6d450ae1ec918f9e37d5b994c22b7d3a1a05
NeilTheSeal <neil.hendren@gmail.com>
2024-02-26 09:19:50 -0700

    Add test for existence

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -2,5 +2,12 @@ require "./spec/spec_helper"
 
 RSpec.describe Attendee do
   before(:each) do
+    @attendee = Attendee.new({ name: "Megan", budget: "$50" })
+  end
+
+  describe "#Initialization - " do
+    it "exists" do
+      expect(@attendee).to be_an_instance_of(Attendee)
+    end
   end
 end
