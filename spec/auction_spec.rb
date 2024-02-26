df1d14267e31c4a2c6f0dff6be005c810f8bd05f
NeilTheSeal <neil.hendren@gmail.com>
2024-02-26 09:26:49 -0700

    Add test for adding items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,6 +3,8 @@ require "./spec/spec_helper"
 RSpec.describe Auction do
   before(:each) do
     @auction = Auction.new
+    @item1 = Item.new("Chalkware Piggy Bank")
+    @item2 = Item.new("Bamboo Picture Frame")
   end
 
   describe "#Initialization - " do
@@ -11,6 +13,14 @@ RSpec.describe Auction do
     end
     it "has attributes" do
       expect(@auction.items).to eq([])
+    end
+  end
+
+  describe "#Adding items - " do
+    it "can add items" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      expect(@auction.items).to eq([@item1, @item2])
     end
   end
 end
